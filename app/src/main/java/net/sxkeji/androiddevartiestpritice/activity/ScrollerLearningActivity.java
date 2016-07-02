70a7f73639de1f7dc4150f06d472154f61d49409
shixinzhang <shixinzhang2016@gamil.com>
2016-07-02 10:56:29 +0800

    add scroller learning

---
+++ app/src/main/java/net/sxkeji/androiddevartiestpritice/activity/ScrollerLearningActivity.java
@@ -0,0 +1,18 @@
+package net.sxkeji.androiddevartiestpritice.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import net.sxkeji.androiddevartiestpritice.R;
+
+/**
+ *
+ * Created by zhangshixin on 6/30/2016.
+ */
+public class ScrollerLearningActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroller);
+    }
+}
